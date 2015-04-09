7090dcac983fc6362549071bc8e4457726d0288d
Borys Pierov <borys.pierov@nih.gov>
2015-04-09 11:10:57 -0400

    [PY-102] Fixed import error

--- setup.py
+++ setup.py
@@ -1,11 +1,10 @@
 from setuptools import setup, find_packages
 
-from packit.config.version.git_pep440 import git_pep440_version_generator
 
 setup(name="packit",
       packages=find_packages('src'),
       package_dir={'': 'src'},
-      version=git_pep440_version_generator(None),
+      version='0.1a',
       entry_points={
           'distutils.setup_keywords': ['packit = packit.core:packit'],
           'setuptools.file_finders': ['packit_extra_files = packit.additional_files:list_files'],
