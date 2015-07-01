5a7960ec35cafc992d71ddd8f0e71bf0505b7678
Michael Kornbluh <kornbluh@ncbi.nlm.nih.gov>
2015-07-01 17:47:21 -0400

    change http to https where possible. SQD-2303

--- bigtests/assert.sh
+++ bigtests/assert.sh
@@ -2,7 +2,7 @@
 # assert.sh 1.1 - bash unit testing framework
 # Copyright (C) 2009-2015 Robert Lehmann
 #
-# http://github.com/lehmannro/assert.sh
+# https://github.com/lehmannro/assert.sh
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Lesser General Public License as published
@@ -15,7 +15,7 @@
 # GNU Lesser General Public License for more details.
 #
 # You should have received a copy of the GNU Lesser General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 export DISCOVERONLY=${DISCOVERONLY:-}
 export DEBUG=${DEBUG:-}
