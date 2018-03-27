1eb1c5ed35afa5b22f77d4e2b0866499bac80af4
Sethi, Amanjeev <amanjeev.sethi@nih.gov>
2018-03-27 15:53:11 -0400

    CD-1450 (misc): pins PBR package to version less than 4.

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
 from setuptools import setup
 
 
-setup(setup_requires=['pbr>=0.10', 'wheel>=0.24', 'glob2==0.4.1'], pbr=True)
+setup(setup_requires=['pbr>=0.10,<4', 'wheel>=0.24', 'glob2==0.4.1'], pbr=True)
